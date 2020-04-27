5377a9d489de4b76ffbf654e95e7451b198c50ed
pc <carrilloricki2211@gmai.com>
2020-04-27 12:26:19 -0500

    Coomint final :v

--- app/src/main/java/com/example/vitality201/Perfil.java
+++ app/src/main/java/com/example/vitality201/Perfil.java
@@ -1,14 +1,99 @@
 package com.example.vitality201;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 public class Perfil extends AppCompatActivity {
+    private Button btn2;
+    TextView Tnombre,Tapellido,Tedad,Tpeso,Tdireccion,Tenfermedad,Temail;
+    DatabaseReference mReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+        btn2  =   findViewById(R.id.button15);
+
+        Tnombre=(TextView) findViewById(R.id.nombreT);
+        Tapellido=(TextView) findViewById(R.id.apellidoT);
+        Tedad=(TextView) findViewById(R.id.edadT);
+        Tpeso=(TextView) findViewById(R.id.pesoT);
+        Tdireccion=(TextView) findViewById(R.id.direccionT);
+        Tenfermedad=(TextView) findViewById(R.id.enfermedadT);
+        Temail=(TextView) findViewById(R.id.emailT2);
+
+
+        String Nombre=getIntent().getStringExtra("nombre");
+        Tnombre.setText(Nombre);
+        String Apellid=getIntent().getStringExtra("apellido");
+        Tapellido.setText(Apellid);
+        String Direccion=getIntent().getStringExtra("direccion");
+        Tdireccion.setText(Direccion);
+        String Enfermedad=getIntent().getStringExtra("enfermedad");
+        Tenfermedad.setText(Enfermedad);
+        String email=getIntent().getStringExtra("email");
+        Temail.setText(email);
+        String Edad= getIntent().getStringExtra("edad");
+        Tedad.setText(Edad);
+        String Peso= getIntent().getStringExtra("peso");
+        Tpeso.setText(Peso);
+
+
+
+
+        mReference = FirebaseDatabase.getInstance().getReference();
+        mReference.child("Usuarios").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+/*
+                if (dataSnapshot.exists()) {
+                    String Dnombre = dataSnapshot.child("nombre").getValue().toString();
+
+                    Tnombre.setText(Dnombre);
+                    String Dapellido = dataSnapshot.child("apellido").getValue().toString();
+                    Tapellido.setText(Dapellido);
+                    String Ddireccion = dataSnapshot.child("direccion").getValue().toString();
+                    Tdireccion.setText(Ddireccion);
+                    String Demail = dataSnapshot.child("email").getValue().toString();
+                    Temail.setText(Demail);
+                    String Denfermedad = dataSnapshot.child("enfermedad").getValue().toString();
+                    Tenfermedad.setText(Denfermedad);
+                    int Dedad = Integer.parseInt(dataSnapshot.child("edad").getValue().toString());
+                    Tedad.setText(Dedad);
+                    int Dpeso = Integer.parseInt(dataSnapshot.child("peso").getValue().toString());
+                    Tpeso.setText(Dpeso);
+
+                }*/
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+        btn2.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(Perfil.this, menu.class));
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/vitality201/registro.java
+++ app/src/main/java/com/example/vitality201/registro.java
@@ -1,14 +1,127 @@
 package com.example.vitality201;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class registro extends AppCompatActivity {
-
+    FirebaseAuth mAu;
+    DatabaseReference mReference;
+    Button mButtonSEnviar;
+    EditText Enombre,Eapellido,Eenfermedad,Epeso,Eedad,Edireccion,Eemail,Epass;
+    private String nombre;
+    private String apellido;
+    private String enfermedad;
+    private int peso;
+    private int edad;
+    private String direccion;
+    private  String email;
+    private  String pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        mButtonSEnviar = findViewById(R.id.btnEnviar);
+        Enombre=findViewById(R.id.etNombre);
+        Eapellido=findViewById(R.id.etApellido);
+        Epeso=findViewById(R.id.etPeso);
+        Eedad=findViewById(R.id.etEdad);
+        Edireccion=findViewById(R.id.etDireccion);
+        Eemail=findViewById(R.id.etEmail);
+        Epass=findViewById(R.id.etPass);
+        Eenfermedad=findViewById(R.id.etEnfermedad);
+        mAu = FirebaseAuth.getInstance();
+        mReference = FirebaseDatabase.getInstance().getReference();
+
+        mButtonSEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nombre = Enombre.getText().toString();
+                email = Eemail.getText().toString();
+                pass = Epass.getText().toString();
+                apellido = Eapellido.getText().toString();
+                peso = Integer.parseInt(Epeso.getText().toString());
+                edad = Integer.parseInt(Eedad.getText().toString());
+                direccion = Edireccion.getText().toString();
+                enfermedad=Eenfermedad.getText().toString();
+
+
+
+                if(!nombre.isEmpty() && !email.isEmpty() && !pass.isEmpty()){
+                    if(pass.length()>=6){
+
+                        registerUser();
+                    }else{
+                        Toast.makeText(registro.this, "El Password debe tenes al menos 6 caracteres", Toast.LENGTH_SHORT).show();
+                    }
+                    registerUser();
+                }else{
+                    Toast.makeText(registro.this, "Debe Completar Los campos", Toast.LENGTH_SHORT).show();
+                }
+
+
+
+
+            }
+        });
+
+
     }
-}
+
+    private void registerUser(){
+        mAu.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Map<String, Object> map=new HashMap<>();
+                    map.put("nombre",nombre);
+                    map.put("email",email);
+                    map.put("password",pass);
+                    map.put("apellido",apellido);
+                    map.put("enfermedad",enfermedad);
+                    map.put("peso",peso);
+                    map.put("edad",edad);
+                    map.put("direccion",direccion);
+
+
+                    String id = mAu.getCurrentUser().getUid();
+                    mReference.child("Usuarios").child(id).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task2) {
+                            if(task2.isSuccessful()){
+                                Intent i =new Intent(registro.this, Perfil.class);
+                                i.putExtra("nombre",Enombre.getText().toString());
+                                i.putExtra("email",Eemail.getText().toString());
+                                i.putExtra("apellido",Eapellido.getText().toString());
+                                i.putExtra("enfermedad",Eenfermedad.getText().toString());
+                                i.putExtra("peso",Integer.parseInt(Epeso.getText().toString()));
+                                i.putExtra("edad",Integer.parseInt(Eedad.getText().toString()));
+                                i.putExtra("direccion",Edireccion.getText().toString());
+                                startActivity(i);
+                                finish();
+                            }else{
+                                Toast.makeText(registro.this, "Ha ocurrido un error!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }}
